ff312b0235df62440e1111bf6a698bae2b6f4977
PBarde <paul.b.barde@gmail.com>
2019-02-13 13:10:40 -0500

    param search

--- P3/main.py
+++ P3/main.py
@@ -7,7 +7,7 @@ import numpy as np
 import torchvision.transforms
 import random
 import os
-from archi import *
+from P3.archi import *
 from torch.autograd import Variable
 import pickle
 
@@ -23,111 +23,66 @@ torch.manual_seed(SEED)
 torch.cuda.manual_seed_all(SEED)
 torch.backends.cudnn.deterministic = True
 
-data_size = (3, 64, 64)
-batch_size = 64
-classes_dict = {0: 'Cat', 1: 'Dog'}
-distrib_means = (0.48973373, 0.45465815, 0.4159738)
-distrib_stds = (0.25206217, 0.24510814, 0.24726307)
-data_augmentation = True
-l2_coeff = 5e-4
-
-# balanced training set as many cats than dogs
-
-base_transforms = [torchvision.transforms.ToTensor(),
-                   torchvision.transforms.Normalize(distrib_means, distrib_stds)]
-r_choice = torchvision.transforms.RandomChoice
-compose = torchvision.transforms.Compose
-
-if data_augmentation:
-    augmentations = [torchvision.transforms.RandomHorizontalFlip(),
-                     torchvision.transforms.RandomResizedCrop(64),
-                     torchvision.transforms.RandomRotation(90),
-                     torchvision.transforms.RandomVerticalFlip()]
-
-    aug_transforms = compose(
-        [torchvision.transforms.RandomApply([r_choice(augmentations +
-                                                      [torchvision.transforms.RandomOrder(augmentations)])],
-                                            p=1.)]
-        + base_transforms)
-else:
-    aug_transforms = compose(base_transforms)
-
-base_transforms = compose(base_transforms)
-
-train_data = torchvision.datasets.ImageFolder(root='./trainset',
-                                              transform=aug_transforms)
-
-test_data = torchvision.datasets.ImageFolder(root='./trainset',
-                                             transform=base_transforms)
-
-len_data = len(train_data)
-# Creating data indices for training and validation splits:
-indices = list(range(len_data))
-id_split = int(0.9 * len_data)
-np.random.shuffle(indices)
-train_indices, test_indices = indices[:id_split], indices[id_split:]
-
-# Creating PT data samplers and loaders:
-train_sampler = SubsetRandomSampler(train_indices)
-test_sampler = SubsetRandomSampler(test_indices)
-
-train_loader = torch.utils.data.DataLoader(
-    train_data, batch_size=batch_size, sampler=train_sampler, num_workers=2,
-    worker_init_fn=random.seed(SEED))
-
-test_loader = torch.utils.data.DataLoader(
-    test_data, batch_size=batch_size, sampler=test_sampler, num_workers=2,
-    worker_init_fn=random.seed(SEED))
-
-# building model
-model_type = 'ResNet'
 cuda = torch.cuda.is_available()
-
 if cuda:
     print('cuda is available')
 else:
     print('cuda is not available')
 
-if model_type == 'MLP':
-    model = nn.Sequential(
-        ResLinear(784, 312),
-        nn.ReLU(),
-        ResLinear(312, 312),
-        nn.ReLU(),
-        ResLinear(312, 10)
-    )
-elif model_type == 'CNN':
-    model = nn.Sequential(
-        nn.Conv2d(3, 16, 5),  # 1 input channel, 16 output channel, 5x5 kernel
-        nn.ReLU(),
-        nn.MaxPool2d(2),
-        nn.Conv2d(16, 16, 5),
-        nn.ReLU(),
-        nn.MaxPool2d(2),
-        Flatten(),
-        ResLinear(2704, 100),
-        nn.ReLU(),
-        ResLinear(100, 10)
-    )
-elif model_type == 'ResNet':
-    model = CIFARResNet18(num_classes=2)
+data_size = (3, 64, 64)
 
-else:
-    raise ValueError
+classes_dict = {0: 'Cat', 1: 'Dog'}
+distrib_means = (0.48973373, 0.45465815, 0.4159738)
+distrib_stds = (0.25206217, 0.24510814, 0.24726307)
 
-if cuda:
-    model = model.cuda()
+def get_loaders(batch_size, data_augmentation):
+    # balanced training set as many cats than dogs
+    base_transforms = [torchvision.transforms.ToTensor(),
+                       torchvision.transforms.Normalize(distrib_means, distrib_stds)]
+    r_choice = torchvision.transforms.RandomChoice
+    compose = torchvision.transforms.Compose
 
-# summary(model,data_size)
-summary(model, (3, 64, 64))
+    if data_augmentation:
+        augmentations = [torchvision.transforms.RandomHorizontalFlip(),
+                         torchvision.transforms.RandomResizedCrop(64),
+                         torchvision.transforms.RandomRotation(90),
+                         torchvision.transforms.RandomVerticalFlip()]
 
-## Setting the optimizer
-num_epochs = 200  # number of training epochs
-lr0 = 0.1
-criterion = nn.CrossEntropyLoss()  # to compute the loss
-optimizer = optim.SGD(model.parameters(), lr=lr0)
-scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor=0.5, patience=3, mode='max')
+        aug_transforms = compose(
+            [torchvision.transforms.RandomApply([r_choice(augmentations +
+                                                          [torchvision.transforms.RandomOrder(augmentations)])],
+                                                p=1.)]
+            + base_transforms)
+    else:
+        aug_transforms = compose(base_transforms)
 
+    base_transforms = compose(base_transforms)
+
+    train_data = torchvision.datasets.ImageFolder(root='./trainset',
+                                                  transform=aug_transforms)
+
+    valid_data = torchvision.datasets.ImageFolder(root='./trainset',
+                                                 transform=base_transforms)
+
+    len_data = len(train_data)
+    # Creating data indices for training and validation splits:
+    indices = list(range(len_data))
+    id_split = int(0.9 * len_data)
+    np.random.shuffle(indices)
+    train_indices, valid_indices = indices[:id_split], indices[id_split:]
+
+    # Creating PT data samplers and loaders:
+    train_sampler = SubsetRandomSampler(train_indices)
+    valid_sampler = SubsetRandomSampler(valid_indices)
+
+    train_loader = torch.utils.data.DataLoader(
+        train_data, batch_size=batch_size, sampler=train_sampler, num_workers=2,
+        worker_init_fn=random.seed(SEED))
+
+    valid_loader = torch.utils.data.DataLoader(
+        valid_data, batch_size=batch_size, sampler=valid_sampler, num_workers=2,
+        worker_init_fn=random.seed(SEED))
+    return train_loader, valid_loader
 
 ## Defining the evaluation routines
 def accuracy(proba, y):
@@ -135,12 +90,10 @@ def accuracy(proba, y):
     return correct / y.size(0)
 
 
-def evaluate(dataset_loader, criterion):
+def evaluate(model, dataset_loader, criterion, model_type):
     LOSSES = 0
     COUNTER = 0
     for batch in dataset_loader:
-        optimizer.zero_grad()
-
         x, y = batch
         if model_type == 'MLP':
             x = x.view(-1, 784)
@@ -161,7 +114,7 @@ def evaluate(dataset_loader, criterion):
     return LOSSES / float(COUNTER)
 
 
-def L2_loss(coeff):
+def L2_loss(model, coeff):
     l = Variable(torch.FloatTensor(1), requires_grad=True)
     if cuda:
         l = l.cuda()
@@ -171,19 +124,55 @@ def L2_loss(coeff):
             l = l + 0.5 * torch.pow(w[1], 2).sum()
     return l * coeff
 
-root_path = './'
-## Defines the train function
-def train_model():
 
+def build_model(model_type):
+
+    if model_type == 'MLP':
+        model = nn.Sequential(
+            ResLinear(784, 312),
+            nn.ReLU(),
+            ResLinear(312, 312),
+            nn.ReLU(),
+            ResLinear(312, 10)
+        )
+    elif model_type == 'CNN':
+        model = nn.Sequential(
+            nn.Conv2d(3, 16, 5),  # 1 input channel, 16 output channel, 5x5 kernel
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+            nn.Conv2d(16, 16, 5),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+            Flatten(),
+            ResLinear(2704, 100),
+            nn.ReLU(),
+            ResLinear(100, 10)
+        )
+    elif model_type == 'ResNet':
+        model = CIFARResNet18(num_classes=2)
+
+    else:
+        raise ValueError
+
+    if cuda:
+        model = model.cuda()
+
+    return model
+
+
+## Defines the train function
+def train_model(train_loader, valid_loader, model, criterion, optimizer, scheduler, root_path, num_epochs, model_type,
+                l2_coeff):
+    cuda = torch.cuda.is_available()
     if not os.path.exists(root_path):
         os.makedirs(root_path)
     LOSSES = 0
     COUNTER = 0
     ITERATIONS = 0
     learning_curve_nll_train = list()
-    learning_curve_nll_test = list()
+    learning_curve_nll_valid = list()
     learning_curve_acc_train = list()
-    learning_curve_acc_test = list()
+    learning_curve_acc_valid = list()
     best_acc = -np.inf
     for e in range(num_epochs):
         print(f'============= EPOCH {e} ========================')
@@ -202,7 +191,7 @@ def train_model():
                 y = y.cuda()
 
             if l2_coeff is not None:
-                loss = criterion(model(x), y) + L2_loss(l2_coeff)
+                loss = criterion(model(x), y) + L2_loss(model, l2_coeff)
 
             else:
                 loss = criterion(model(x), y)
@@ -214,45 +203,64 @@ def train_model():
             LOSSES += loss.sum().data.cpu().numpy() * n
             COUNTER += n
             ITERATIONS += 1
-            if ITERATIONS % (store_every / 5) == 0:
-                avg_loss = LOSSES / float(COUNTER)
-                LOSSES = 0
-                COUNTER = 0
-                print(" Iteration {}: TRAIN {}".format(
-                    ITERATIONS, avg_loss))
 
-            if ITERATIONS % (store_every) == 0:
+        avg_loss = LOSSES / float(COUNTER)
+        LOSSES = 0
+        COUNTER = 0
+        print(" Epoch {}: TRAIN {}".format(
+            e, avg_loss))
 
-                train_loss = evaluate(train_loader, criterion)
-                learning_curve_nll_train.append(train_loss)
-                test_loss = evaluate(test_loader, criterion)
-                learning_curve_nll_test.append(test_loss)
+        if e % (store_every) == 0:
+            train_loss = evaluate(model, train_loader, criterion, model_type)
+            learning_curve_nll_train.append(train_loss)
+            valid_loss = evaluate(model, valid_loader, criterion, model_type)
+            learning_curve_nll_valid.append(valid_loss)
 
-                train_acc = evaluate(train_loader, accuracy)
-                learning_curve_acc_train.append(train_acc)
-                test_acc = evaluate(test_loader, accuracy)
-                learning_curve_acc_test.append(test_acc)
-                if round(test_acc, 3) > best_acc:
-                    best_acc = round(test_acc, 3)
-                    path_to_best = root_path + 'd_aug' * int(data_augmentation) + f'_test_acc_{best_acc}.pth'
-                    torch.save(model, path_to_best)
-                    print('saved model')
+            train_acc = evaluate(model, train_loader, accuracy, model_type)
+            learning_curve_acc_train.append(train_acc)
+            valid_acc = evaluate(model, valid_loader, accuracy, model_type)
+            learning_curve_acc_valid.append(valid_acc)
+            if round(valid_acc, 3) > best_acc:
+                best_acc = round(valid_acc, 3)
+                path_to_best = root_path + 'd_aug' * int(data_augmentation) + f'_valid_acc_{best_acc}.pth'
+                torch.save(model, path_to_best)
+                print('saved model')
 
-                print(" [NLL] TRAIN {} / TEST {}".format(
-                    train_loss, test_loss))
-                print(" [ACC] TRAIN {} / TEST {}".format(
-                    train_acc, test_acc))
+            print(" [NLL] TRAIN {} / valid {}".format(
+                train_loss, valid_loss))
+            print(" [ACC] TRAIN {} / valid {}".format(
+                train_acc, valid_acc))
 
-                scheduler.step(test_acc)
+            scheduler.step(valid_acc)
+
+
+    fp = open(root_path + 'summary.pckl', 'wb')
+    pickle.dump([learning_curve_nll_train, learning_curve_nll_valid,
+                 learning_curve_acc_train, learning_curve_acc_valid, path_to_best], fp)
 
     return learning_curve_nll_train, \
-           learning_curve_nll_test, \
+           learning_curve_nll_valid, \
            learning_curve_acc_train, \
-           learning_curve_acc_test, path_to_best
+           learning_curve_acc_valid,
 
 
-store_every = 1000
-nll_train, nll_test, acc_train, acc_test, p_best = train_model()
+if __name__== '__main__':
+    batch_size = 64
+    data_augmentation = True
+    model_type = 'CNN'
+    train_loader, valid_loader = get_loaders(batch_size, data_augmentation)
+    store_every = 1
+    lr0 = 0.1
+    num_epochs = 2  # number of training epochs
+    l2_coeff_list = [None, 1e-3, 1e-4]
+    criterion = nn.CrossEntropyLoss()  # to compute the loss
 
-fp=open(root_path + 'summary.pckl','wb')
-pickle.dump([nll_train, nll_test, acc_train, acc_test, p_best], fp)
+    for l2_coeff in l2_coeff_list:
+        root_path = f'./l2_{l2_coeff}/'
+        model = build_model(model_type)
+        optimizer = optim.SGD(model.parameters(), lr=lr0)
+        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor=0.5, patience=3, mode='max')
+        train_model(train_loader, valid_loader, model, criterion, optimizer, scheduler, root_path,
+                    num_epochs, model_type, l2_coeff)
+
+
